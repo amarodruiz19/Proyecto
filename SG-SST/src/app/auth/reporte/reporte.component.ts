f6f1f007fc66aa07c0e61eb55655a532cddb4574
Oscar Mauricio González <omgonzalezq@gmail.com>
2021-08-10 22:05:48 -0500

    10/08/21

--- SG-SST/src/app/auth/reporte/reporte.component.ts
+++ SG-SST/src/app/auth/reporte/reporte.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
-import { FormBuilder } from '@angular/forms'; 
-import { Validators } from '@angular/forms'; 
-import { FormArray } from '@angular/forms'; 
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+
+
+interface tipoRep {
+  value: string;
+  viewValue: string;
+}
 
 
 @Component({
@@ -11,6 +17,11 @@ import { FormArray } from '@angular/forms';
   styleUrls: ['./reporte.component.css']
 })
 export class ReporteComponent implements OnInit {
+  tipoReps: tipoRep[] = [
+    {value: 'incidente-0', viewValue: 'Incidente'},
+    {value: 'riesgo-1', viewValue: 'Riesgo'},
+    {value: 'accidente-2', viewValue: 'Accidente'}
+  ];
 
   constructor(private fb: FormBuilder,private http: HttpClient) { }
   profileForm = this.fb.group({
@@ -22,20 +33,27 @@ export class ReporteComponent implements OnInit {
     descripcionRep: ['', Validators.required],
     idDepto: ['', Validators.required],
     ubicacionRep : ['', Validators.required],
-    idFoto : ['', Validators.required],
-    idTipoRep : ['', Validators.required],
-    idMatrizRep : ['', Validators.required],
+/*     idFoto : ['', Validators.required], */
+    // tipoRep : ['', Validators.required],
+    idMatrizRep : ['', Validators.required]
     });
 
+
+
+    /* @Component({
+      selector: 'select-overview-example',
+      templateUrl: 'select-overview-example.html',
+    }) */
+/*     export class SelectOverviewExample {
+
+    } */
+
+
   onSubmit (){
-    console.warn(this.profileForm.value); 
-    this.http.post('http://localhost:8080/api/reporte', 
-    this.profileForm.value).subscribe( (response) => console.log(response), (error) => console.log(error) ) 
+    console.warn(this.profileForm.value);
+    this.http.post('http://localhost:8080/api/reporte',
+    this.profileForm.value).subscribe( (response) => console.log(response), (error) => console.log(error) )
   }
   ngOnInit(): void {
   }
-
-
-
-
 }
